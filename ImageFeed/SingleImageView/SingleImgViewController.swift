4620c8a6c6922fb8921302250618ec9de956b2af
Vegicode <vadimegiazarov@mail.ru>
2024-07-24 14:16:42 +0300

    Добавили ж

---
+++ ImageFeed/SingleImageView/SingleImgViewController.swift
@@ -0,0 +1,12 @@
+//
+//  SingleImageViewController.swift
+//  ImageFeed
+//
+//  Created by Mac on 24.07.2024.
+//
+
+import UIKit
+
+final class SingleImageViewController: UIViewController {
+    
+}
