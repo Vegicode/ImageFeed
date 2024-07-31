2f7213f1d2bfda3cd4404ba0c214734ae4ad3666
Vegicode <vadimegiazarov@mail.ru>
2024-07-31 21:09:35 +0300

    Выполнил 9 сприт

--- ImageFeed/Profile/ProfileViewController.swift
+++ ImageFeed/Profile/ProfileViewController.swift
@@ -2,19 +2,55 @@
 import UIKit
 
 final class ProfileViewController: UIViewController{
-    @IBOutlet var avatarImageView: UIImageView!
-    @IBOutlet var name: UILabel!
-    @IBOutlet var loginLabel: UILabel!
-    @IBOutlet var logoutButton: UIButton!
-    @IBOutlet var descriptionLabel: UILabel!
     
+    private var label: UILabel?
     
-    
-    
-    @IBAction func didTapLogoutButton(_ sender: Any) {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        let profileImage = UIImage(named: "Image")
+        let imageView = UIImageView(image: profileImage)
+        imageView.tintColor = .gray
+        imageView.translatesAutoresizingMaskIntoConstraints = false
+        view.addSubview(imageView)
+        NSLayoutConstraint.activate([
+            imageView.widthAnchor.constraint(equalToConstant: 70),
+            imageView.heightAnchor.constraint(equalToConstant: 70),
+            imageView.leadingAnchor.constraint(equalTo: view.safeAreaLayoutGuide.leadingAnchor, constant: 20),
+            imageView.topAnchor.constraint(equalTo: view.safeAreaLayoutGuide.topAnchor, constant: 20),
+        
+        ])
+        
+        let label = UILabel()
+        label.text = "Name"
+        label.textColor = .white
+        label.translatesAutoresizingMaskIntoConstraints = false
+        view.addSubview(label)
+        label.leadingAnchor.constraint(equalTo: imageView.leadingAnchor).isActive = true
+        label.topAnchor.constraint(equalTo: imageView.bottomAnchor, constant: 20).isActive = true
+        self.label = label
+        
+        
+        
+        let button = UIButton.systemButton(
+            with: UIImage(systemName: "ipad.and.arrow.forward")!,
+            target: self,
+            action: #selector(Self.didTapLogoutButton)
+        )
+        button.tintColor = .red
+        button.translatesAutoresizingMaskIntoConstraints = false
+        view.addSubview(button)
+        button.trailingAnchor.constraint(equalTo: view.safeAreaLayoutGuide.trailingAnchor, constant: -20).isActive = true
+        button.centerYAnchor.constraint(equalTo: imageView.centerYAnchor).isActive = true
     }
+    @objc
+            private func didTapLogoutButton() {
+                label?.removeFromSuperview()
+                      
+                label = nil
+            }
     
-   
 }
 
 
