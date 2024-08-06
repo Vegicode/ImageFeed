28553428960328f028e134f624c1b9965d11e129
Vegicode <vadimegiazarov@mail.ru>
2024-08-06 22:24:01 +0300

    Проблема в 10 спринте

---
+++ Auth/AuthViewController.swift
@@ -0,0 +1,12 @@
+//
+//  AuthViewController.swift
+//  ImageFeed
+//
+//  Created by Mac on 06.08.2024.
+//
+
+import Foundation
+
+class AuthViewController{
+    
+}
---
+++ Auth/WebViewViewController.swift
@@ -0,0 +1,12 @@
+//
+//  WebViewViewController.swift
+//  ImageFeed
+//
+//  Created by Mac on 06.08.2024.
+//
+
+import Foundation
+
+class WebViewViewController{
+    
+}
