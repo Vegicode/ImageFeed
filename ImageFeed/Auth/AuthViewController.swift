d4eca1d7df9ef7c770efdc1579856896780fb654
Vegicode <vadimegiazarov@mail.ru>
2024-08-19 11:10:33 +0300

    Задача 10 спринта

--- ImageFeed/Auth/AuthViewController.swift
+++ ImageFeed/Auth/AuthViewController.swift
@@ -7,6 +7,28 @@
 
 import UIKit
 
-class AuthViewController: UIViewController{
+protocol AuthViewControllerDelegate: AnyObject {
+    func authViewController(_ vc: AuthViewController, didAuthenticateWithCode code: String)
+}
+
+
+final class AuthViewController: UIViewController{
+    weak var delegate: AuthViewControllerDelegate?
+    
+    private let ShowWebViewSegueIdentifier = "ShowWebView"
+}
+
+extension AuthViewController: WebViewControllerDelegate {
+    func webViewViewController(_ vc: WebViewViewController, didAuthenticateWithCode code: String) {
+        
+        delegate?.authViewController(self, didAuthenticateWithCode: code)
+        
+        
+    }
+    
+    func webViewViewControllerDidCancel(_ vc: WebViewViewController) {
+        dismiss(animated: true)
+    }
+    
     
 }
---
+++ ImageFeed/OAuth2/OAuth2TokenStorage.swift
@@ -0,0 +1,12 @@
+//
+//  OAuth2TokenStorage.swift
+//  ImageFeed
+//
+//  Created by Mac on 17.08.2024.
+//
+
+import Foundation
+
+final class OAuth2TokenStorage {
+    var token: String?
+}
