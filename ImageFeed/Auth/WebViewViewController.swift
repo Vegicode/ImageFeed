16066cf207b74f2c3c7f85029531f1fecffc34ed
Vegicode <vadimegiazarov@mail.ru>
2024-08-23 13:35:30 +0300

    Внес правки в 10 спринт(2)

--- ImageFeed/Auth/WebViewViewController.swift
+++ ImageFeed/Auth/WebViewViewController.swift
@@ -8,6 +8,8 @@
 import UIKit
 import WebKit
 
+fileprivate let unsplashAuthorizeURLString = "https://unsplash.com/oauth/authorize"
+
 protocol WebViewViewControllerDelegate: AnyObject {
     func webViewViewController(_ vc: WebViewViewController, didAuthenticateWithCode code: String)
     func webViewViewControllerDidCancel(_ vc: WebViewViewController)
@@ -21,11 +23,13 @@ final class WebViewViewController: UIViewController{
     
     override func viewDidLoad() {
         super.viewDidLoad()
+        
+        webView.navigationDelegate = self
+
         configureBackButton()
         loadWebView()
         
-        webView.navigationDelegate = self
-       
+
     }
     
     private func configureBackButton() {
@@ -70,6 +74,7 @@ extension WebViewViewController: WKNavigationDelegate {
         
         if let code = fetchCode(url: navigationAction.request.url) {
             delegate?.webViewViewController(self, didAuthenticateWithCode: code)
+            print("DEBUG:", "WebViewViewController Delegate called")
             decisionHandler(.cancel)
         } else {
             decisionHandler(.allow)
@@ -80,32 +85,36 @@ extension WebViewViewController: WKNavigationDelegate {
 private extension WebViewViewController {
     func loadWebView(){
         
-        var components = URLComponents(string: "https://unsplash.com/oauth/authorize")
-        components?.queryItems = [
+        guard var components = URLComponents(string: unsplashAuthorizeURLString) else {
+            print("Ошибка")
+            return
+        }
+        components.queryItems = [
             URLQueryItem(name: "client_id", value: Constants.accessKey),
             URLQueryItem(name: "redirect_uri", value: Constants.redirectURI),
             URLQueryItem(name: "response_type", value: "code"),
             URLQueryItem(name: "scope", value: Constants.accessScope)
         ]
         
-        if let url = components?.url{
-            
-            let request = URLRequest(url: url)
-            webView.load(request)
+        guard let url = components.url else{
+            print("Ошибка")
+            return
         }
+        let request = URLRequest(url: url)
+        webView.load(request)
     }
+    
     
     func fetchCode(url: URL?) -> String?{
         guard let url = url,
               let components = URLComponents(string: url.absoluteString),
               components.path == "/oauth/authorize/native",
               let item = components.queryItems?.first(where: {$0.name == "code"}) else { return nil }
-              
+        
         return item.value
         
     }
-    
+}
 
     
-    
-}
+
