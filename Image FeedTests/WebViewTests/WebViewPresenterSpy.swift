6f37ebd076549e17510451f036e40e78bfdd783c
Vegicode <vadimegiazarov@mail.ru>
2024-10-04 20:24:12 +0300

    Добавил недостающие файлы

---
+++ Image FeedTests/WebViewTests/WebViewPresenterSpy.swift
@@ -0,0 +1,25 @@
+//
+//  WebViewPresenterSpy.swift
+//  Image FeedTests
+//
+//  Created by Mac on 04.10.2024.
+//
+@testable import ImageFeed
+import Foundation
+
+final class WebViewPresenterSpy: WebViewPresenterProtocol {
+    var viewDidLoadCalled: Bool = false
+    var view: WebViewViewControllerProtocol?
+    
+    func viewDidLoad() {
+        viewDidLoadCalled = true
+    }
+    
+    func didUpdateProgressValue(_ newValue: Double) {
+    
+    }
+    
+    func code(from url: URL) -> String? {
+        return nil
+    }
+}
